70623e77edf11b2d46bdfadbbbe859af0c9c3c80
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2022-11-20 11:04:04 -0800

    added code to get user input from the local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -76,7 +76,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
    //grabbing the timeblock(id) and its text content(.description) from local storage via the block's id name
-
+   $('#hour9 .description').val(localStorage.getItem('hour9'));
+   $('#hour10 .description').val(localStorage.getItem('hour10'));
+   $('#hour11 .description').val(localStorage.getItem('hour11'));
+   $('#hour12 .description').val(localStorage.getItem('hour12'));
+   $('#hour13 .description').val(localStorage.getItem('hour13'));
+   $('#hour14 .description').val(localStorage.getItem('hour14'));
+   $('#hour15 .description').val(localStorage.getItem('hour15'));
+   $('#hour16 .description').val(localStorage.getItem('hour16'));
+   $('#hour17 .description').val(localStorage.getItem('hour17'));
 
 
   // TODO: Add code to display the current date in the header of the page.
