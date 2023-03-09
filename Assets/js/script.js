14227269ce56ad933f112ddf05013f0185f75e33
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-03-08 17:28:08 -0800

    refactored code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,20 +5,20 @@ $(function () {
 
   // function saving input content onto local storage
   $('.saveBtn').on('click', function () { //setting an event listener to the saveBtn class on click
-    var text = $(this).siblings('.description').val(); // recieving informatio from the textarea element because it has the class of 'description'
-    var time = $(this).parent().attr('id'); //recieving the changges made from it's relative time block // the id will be used to pull information from the local storage onto user interface
+    let text = $(this).siblings('.description').val(); // recieving informatio from the textarea element because it has the class of 'description'
+    let time = $(this).parent().attr('id'); //recieving the changges made from it's relative time block // the id will be used to pull information from the local storage onto user interface
     localStorage.setItem(time, text); //linking both timeblock value and text value to local storage
   });
 
   function hourTracker () {
-    var currentHour = dayjs().hour();
+    let nowHour = dayjs().hour();
   
       $('.timeBlock').each(function () {
-        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+        let hourBlock = parseInt($(this).attr("id").split("hour")[1]);
   
-        if (hourBlock > currentHour) {
+        if (hourBlock > nowHour) {
           $(this).addClass("future");
-        } else if (hourBlock === currentHour) {
+        } else if (hourBlock === nowHour) {
           $(this).addClass("present");
           $(this).removeClass("past");
           $(this).removeClass("future");
@@ -47,13 +47,12 @@ $(function () {
 
   // code to display the current date in the header of the page.
   function timeDisplay() {
-    var timeDisplayEl = $('#currentDay');
-    var rightNow = dayjs().format('dddd, MMMM DD YYYY. h:mm:ss');
-    timeDisplayEl.text(rightNow);
+    let timeDisplay = $('#currentDay');
+    let currentTime = dayjs().format('dddd, MMMM DD YYYY. h:mm:ss');
+    timeDisplay.text(currentTime);
   };
+
   timeDisplay();
   setInterval(timeDisplay, 1000);
-
-
-
+  
 });
--- README.md
+++ README.md
@@ -17,7 +17,9 @@
 
   This web application takes a mobile first approach, though it is optimized for larger screens as well. The user is able to enter various tasks for their day onto it's constituent time block and is then able to save their task using the 'save' button to the right of each time block. Their information will be stored onto their system's local storage. When the user refreshes the page, the task will persist.  
 
-  ![img](./Assets/images/workday-scheduler-screenshot.png)
+  ![127 0 0 1_5500_index html](https://user-images.githubusercontent.com/115049801/223891131-82197df4-f1cc-40bf-87a8-391849671296.png)
+
+  
   ## License
   This app is covered under the [MIT License](https://opensource.org/licenses/MIT)<br>
   Copyright (C) Matthew Delgado 2023
